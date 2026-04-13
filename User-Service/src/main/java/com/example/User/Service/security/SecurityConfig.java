7051699b229d81f1043b9e52e7b7f3df308510b4
ReFL21 <icaka2119@gmail.com>
2026-04-13 17:49:41 +0200

    successfully tested creation funcitonality with 2 users

--- User-Service/src/main/java/com/example/User/Service/security/SecurityConfig.java
+++ User-Service/src/main/java/com/example/User/Service/security/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("http://localhost:5173"));
+//        cfg.setAllowedOrigins(List.of("http://localhost:5173"));
         cfg.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         cfg.setAllowedHeaders(List.of("*"));
         cfg.setAllowCredentials(true);
@@ -51,7 +51,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         // allow both registration and login without a token
-                        .requestMatchers("/users/register", "/users/login").permitAll()
+                        .requestMatchers("/user/register", "/user/login").permitAll()
                         // everything else needs a valid JWT
                         .anyRequest().authenticated()
                 )
